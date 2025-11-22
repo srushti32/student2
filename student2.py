c9932c286245c30fc1ca606af6db7ae2b5c05aab
srushti32 <hsrushti320@gmail.com>
2025-11-22 12:19:23 +0530

    Update student2.py

--- student2.py
+++ student2.py
@@ -5,7 +5,7 @@ if len(sys_argv) == 3:
   name = sys_argv[1]
   rollno = sys.argv[2]
   print("enter provided input values:")
-else
+else :
   script_name = sys.argv[0]
   namec = "srushti"
   rollno = "260"
